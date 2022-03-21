47133d046cd560916b8d38438338d783842ca896
Miki Leardini <mikileardini96@gmail.com>
2022-03-21 10:03:19 +0100

    Fine
    Conclusione sito per progetto

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,31 @@ function update_carousel_words(){
     }
 }
 
-// menu navbar
+// animation 
 $(document).ready(function(){
+
+//scroll up btn show/hide
+    $(window).scroll(function(){
+        if (this.scrollY > 500){
+            $(".scroll-up-btn").addClass("show");
+        }else{
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
+
+    // scroll up btn slide up
+    $(".scroll-up-btn").click(function(){
+        $("html").animate({scrollTop: 0});
+        //remove scroll behavior
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    //menu icon mobile
 
     $(".menu-mobile").click(function(){
         $(".navbar .max-width .menu").toggleClass("active");
